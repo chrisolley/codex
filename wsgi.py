9020cb811fea7d38fcce3f1af8cc9a0879516292
chrisolley <olley.cj@gmail.com>
2021-04-18 18:39:21 -0700

    updated host

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
 from app import create_app
+import os
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=os.environ.get("PORT", 8000), debug=True)
